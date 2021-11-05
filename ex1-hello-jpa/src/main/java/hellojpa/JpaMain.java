d8ad3966cfc9f6c440ceb42ff097112758839e6b
soeunkk <luckyber1@naver.com>
2021-11-05 12:15:08 +0900

    엔티티 매핑
    자바 ORM 표준 JPA 프로그래밍 - 기본편 (김영한)
    Chapter_04 엔티티 매핑
    Part_01 객체와 테이블 매핑
    Part_02 데이터베이스 스키마 자동 생성
    Part_03 필드와 컬럼 매핑
    Part_04 기본 키 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,54 +12,30 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
-        EntityTransaction transaction1 = em.getTransaction();
-        transaction1.begin();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
         try {
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            //영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member); //쿼리문이 Before 과 After 사이에 출력 되지 않고 나중에 출력됨 -> 커밋 시점에 쿼리를 날림
-            //em.detach(member);  //detach(): 영속성 컨테이너에서 엔티티를 지움
-            System.out.println("=== AFTER ===");
+            Member member2 = new Member();
+            member2.setUsername("B");
 
-            //1차 캐시 있는 엔티티 조회
-            Member findMember = em.find(Member.class, 100L);    //위에서 엔티티를 만들 때 1차 캐시에 저장함 -> 1차 캐시에서 가져오므로 조회 쿼리를 날리지 않음
+            Member member3 = new Member();
+            member3.setUsername("C");
 
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
-
-            //1차 캐시에 없는 엔티티 2번 조회
-            Member findMember1 = em.find(Member.class, 1L);    //1차 캐시에 없는 1L에 대해 첫번째 조회를 할 때는 DB 에서 가져옴 (조회 쿼리를 날림)
-            Member findMember2 = em.find(Member.class, 1L);    //두번째 조회부터는 1차 캐시에서 가져옴 (조회 쿼리를 날리지 않음)
-
-            //영속성 엔티티 동일성 보장
-            System.out.println(findMember1 == findMember2); //true
-
-            //트랜잭션을 지원하는 쓰기 지연
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
+            //SEQUENCE 경우,
+            //DB SEQ = 1    |   1 (추가할 수 있는 값이 없어서 50을 늘려서 다시 설정함)
+            //DB SEQ = 51   |   2
             em.persist(member1);
-            em.persist(member2);
-            System.out.println("=======================");  //이 출력 이후에 member1, member2에 대한 생성 쿼리를 날림
+            em.persist(member2);    //MEM
+            em.persist(member3);    //MEM
 
-            member1.setName("C");   //1차 캐시에 있는 엔티티가 수정되면, 커밋 시, 스냅샷과 비교하여 자동으로 DB에 업데이트 쿼리를 날림
 
-            //플러시
-            Member member3 = new Member(200L, "member200");
-            em.persist(member3);
-
-            em.flush(); //플러시 매커니즘이 즉시 일어남
-            System.out.println("======== FLUSH ========");  //이 출력 이전에 생성, 업데이트 쿼리를 날림
-
-            transaction1.commit();  //커밋하는 이 시점에, 영속성 컨텍스트에 저장된 엔티티를 DB에 저장함 (쿼리를 돌림)
+            tx.commit();
         } catch (Exception e) {
-            transaction1.rollback();
+            tx.rollback();
         } finally {
             em.close();
         }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,38 +1,41 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
-    @Id
-    private Long id;
-    private String name;
+    @Id //PK로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "name")  //객체는 username 이라고 사용하지만 DB 에는 컬럼명 name 으로 저장
+    private String username;
+
+//    private Integer age;    //DB에 Integer 와 가장 적절한 숫자 타입으로 저장됨
+//
+//    @Enumerated(EnumType.STRING)    //자바에서 enum 타입으로 사용하는 필드를 DB에 저장
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)   //TemporalType 에는 3가지가 있음 (TIMESTAMP:날짜+시간, DATE:날짜, TIME:시간)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    @Lob    //varchar 를 넘어서는 큰 컨텐츠 저장 (String 의 경우 CLOB 으로 저장됨)
+//    private String description;
+//
+//    @Transient  //스프링 내부에서만 사용하는 필드 (필드에 아무런 어노테이션을 사용하지 않으면 DB에 있는 컬럼과 매핑되어야 함)
+//    private int temp;
 
     public Member() {   //JPA 에서는 내부에서 동적으로 객체를 생성하기 때문에 기본 생성자가 필요함
 
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
