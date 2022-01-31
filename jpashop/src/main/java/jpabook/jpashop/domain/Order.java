b25683e368599a1aca99699a9891dbb3b7233eb4
soeunkk <luckyber1@naver.com>
2022-01-31 22:06:30 +0900

    [feat] 주문, 주문상품 엔티티 비즈니스 로직 추가 (#3)
    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 (김영한)
    Chapter_06 주문 도메인 개발
    Part_01 주문, 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -36,7 +36,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;    //주문상태 [ORDER, CANCEL]
 
-    //연관관계 편의 메서드
+    //==연관관계 편의 메서드==//
     public void setMember(Member member) {  //Order 객체에도 값을 넣어주고 Member 객체에도 값을 넣어줘야 하는데 메서드 하나로 정리
         this.member = member;
         member.getOrders().add(this);
@@ -50,5 +50,43 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(orderItem -> orderItem.getItem().getPrice())
+                .sum();
     }
 }
