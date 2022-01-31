0c545c6dddda2217c2354f0e413b5c514ece16ea
soeunkk <luckyber1@naver.com>
2022-01-31 16:55:35 +0900

    [remove] 사용하지 않는 예제코드 삭제
    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 (김영한)
    Chapter_04 회원 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,10 +8,6 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
 	}
 
 }
